f3d7dfb9c9b3d6aa1e39b6d142ff3ce9bee09474
ikostenevich <ikostenevich.dev@gmail.com>
2025-03-03 19:19:24 +0200

    feat: add user model

--- src/modules/auth/account/account.service.ts
+++ src/modules/auth/account/account.service.ts
@@ -1,5 +1,7 @@
 import { PrismaService } from '@/src/core/prisma/prisma.service';
 import { Injectable } from '@nestjs/common';
+import { CreateUserInput } from './inputs/create-user.input';
+import { hash } from 'argon2';
 
 @Injectable()
 export class AccountService {
@@ -8,5 +10,40 @@ export class AccountService {
   public async findAll() {
     const users = await this.prismaService.user.findMany()
     return users
+  }
+
+  public async create(input: CreateUserInput) {
+    const {username, email, password} = input
+
+    const isUsernameExists = await this.prismaService.user.findUnique({
+      where: {
+        username
+      }
+    })
+
+    if(isUsernameExists) {
+      throw new Error('Username already exists')
+    }
+
+    const isEmailExists = await this.prismaService.user.findUnique({
+      where: {
+        email
+      }
+    })
+
+    if(isEmailExists) {
+      throw new Error('Email already exists')
+    }
+
+    await this.prismaService.user.create({
+      data: {
+        username,
+        email,
+        password: await hash(password),
+        displayName: username
+      }
+    })
+
+    return true
   }
 }
